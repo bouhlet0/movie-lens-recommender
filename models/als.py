e99b629dd3d981b9ca7f79d540e497193849aedc
bouhlet0 <bouhlet0@gmail.com>
2026-04-21 16:58:36 +0200

    Added ALS model to baseline and base LLN ranking and testing

--- models/als.py
+++ models/als.py
@@ -17,16 +17,31 @@ class ALSModel(BaseRecommender):
 
     supports_ranking: bool = True
 
+    _user_factors: np.ndarray | None
+    _item_factors: np.ndarray | None
+    _seen: dict[int, set[int]]
+    _n_items: int
     def __init__(
         self,
         factors: int = 50,
         iterations: int = 20,
         regularization: float = 0.01,
-        use_gpu: bool = False,
+        use_gpu: bool | None = None,
+        alpha: float = 1.0,
+        
     ) -> None:
         self.factors = factors
         self.iterations = iterations
         self.regularization = regularization
+
+        if use_gpu is None:
+            try:
+                import cupy
+                print("Importing cupy")
+                use_gpu = True
+            except ImportError:
+                use_gpu = False
+        self.use_gpu = use_gpu
 
         self._model = AlternatingLeastSquares(
             factors=factors,
@@ -34,19 +49,26 @@ class ALSModel(BaseRecommender):
             regularization=regularization,
             use_gpu=use_gpu,
         )
-        self._user_factors: np.ndarray = None
-        self._item_factors: np.ndarray = None
-        self._seen: dict[int, set[int]] = {}
-        self._n_items: int = 0
+        self._user_factors = None
+        self._item_factors = None
+        self._seen = {}
+        self._n_items = 0
 
     def fit(self, train_df: pl.DataFrame, implicit_matrix: sp.csr_matrix) -> None:
         self._seen = build_seen_items(train_df)
         self._n_items = implicit_matrix.shape[1]
+        confidence_matrix = (implicit_matrix * self.alpha).astype(np.float32)
+        self._model.fit(confidence_matrix.T.tocsr())
 
-        self._model.fit(implicit_matrix.T.tocsr())
+        uf  = self._model.user_factors
+        itf = self._model.item_factors
+        if self.use_gpu:
+            print("Using gpu")
+            uf  = uf.to_numpy()
+            itf = itf.to_numpy()
 
-        self._user_factors = self._model.user_factors
-        self._item_factors = self._model.item_factors
+        self._user_factors = itf.astype(np.float32) # Implicit quirk fix
+        self._item_factors = uf.astype(np.float32) # Implicit quirk fix
 
     def predict(self, eval_df: pl.DataFrame) -> np.ndarray:
         raise NotImplementedError("ALSModel is a ranking model and does not support rating prediction.")
